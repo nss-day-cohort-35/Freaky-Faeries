c46da5414298f15e09cf42072597faabeb2050ec
lauren maxwell <laurenelizamax@icloud.com>
2019-10-10 11:51:12 -0500

    worked on search bar

--- src/components/friend/FriendAddForm.js
+++ src/components/friend/FriendAddForm.js
@@ -1,4 +1,5 @@
 // import React, { Component } from 'react';
+// import FriendManager from '../../modules/FriendManager';
 
 // class FriendAddForm extends Component {
 //     state = {
@@ -15,43 +16,50 @@
 
 //     getData = () => {
 //         fetch(`http://localhost:5002/users`)
-//         .then(response => response.json())
-//         .then(responseData => {
-//             // console.log(responseData)
-//             this.setState({
-//                 users:responseData
+//             .then(response => response.json())
+//             .then(responseData => {
+//                 // console.log(responseData)
+//                 this.setState({
+//                     users: responseData
+//                 })
 //             })
-//         })
 //     }
 
 //     filterArray = () => {
 //         var searchString = this.state.query;
 //         var responseData = this.state.users;
-//         if(searchString.length > 0){
-//             responseData = responseData.filter(l => {
-//             })
-//         }
+//             if (searchString.length > 0) {
+
+//                 /* when user presses enter, find matching concept */
+//                 FriendManager.getAll()
+//                     .then(res => res.filter(data => data.name.includes(onChange.target.value)))
+//                     .then(res =>
+//                         responseData = responseData.filter(l => {
+//                             )}
+//                             )
+//                     }
 //     }
 
-//     componentWillMount() {
-//         this.getData();
-//     }
-//     render() {
-//         return (
-//             <div className="searchFriendForm">
-//                 <form>
-//                     <input type="text" id="filter" placeholder="Search for Friends" ref={input => this.search = input} onChange={this.handleInputChange}/>
-//                 </form>
-//                 <div>
-//                     {
-//                         this.state.users.map((i) =>
-//                             <p>{i.name}</p>
-//                         )
-//                     }
-//                 </div>
+
+// componentWillMount() {
+//     this.getData();
+// }
+// render() {
+//     return (
+//         <div className="searchFriendForm">
+//             <form>
+//                 <input type="text" id="filter" placeholder="Search for Friends" ref={input => this.search = input} onChange={this.handleInputChange} />
+//             </form>
+//             <div>
+//                 {
+//                     this.state.users.map((i) =>
+//                         <p>{i.name}</p>
+//                     )
+//                 }
 //             </div>
-//         )
-//     }
+//         </div>
+//     )
+// }
 // }
 
 
