f0a06cdf83042fd07fa5791771ee69960baba45c
Chandler Turner <chandlerturner@Chandlers-MacBook-Air.local>
2019-10-14 10:23:06 -0500

    Commented friends out

--- src/components/friend/FriendCard.js
+++ src/components/friend/FriendCard.js
@@ -4,14 +4,14 @@ class FriendCard extends Component {
     render() {
         return (
             <>
-                <div className="friendCard">
+                {/* <div className="friendCard">
                     <div className="friendCardContent">
                         <p>Name:</p>
                         <img></img>
                         <button className="friendAddBtn" id="addBtn" type="button">Add Friend</button>
                         <button className="friendDeleteBtn" id="deleteBtn" type="button">Delete Friend</button>
                     </div>
-                </div>
+                </div> */}
             </>
         )
     }
